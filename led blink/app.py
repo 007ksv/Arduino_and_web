b73ecffbf2986afedcc394c221e286ad46b0439e
Keshav Sain <46397837+007ksv@users.noreply.github.com>
2020-08-13 19:58:06 +0530

    adding comments

--- led blink/app.py
+++ led blink/app.py
@@ -1,7 +1,10 @@
+'''
+Controlling arduino using Web interface 
+'''
 from flask import Flask, render_template ,request
 import serial
 
-ar=serial.Serial('/dev/ttyACMO',9600)
+ar=serial.Serial('/dev/ttyACMO',9600)           #used for serial communication  
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'keshavsain'
@@ -9,10 +12,10 @@ app.config['SECRET_KEY'] = 'keshavsain'
 @app.route('/', methods=['GET', 'POST'])
 def index():
         if request.method=='POST':
-                result = request.form.get('state')
+                result = request.form.get('state')              #getting state of led using http request data
                 if result == 'on':
                         print(result)
-                        ar.write('1')
+                        ar.write('1')                           #sending serial data to arduino
                         return render_template('index.html', msg=result)
                 elif result == 'off':
                         print(result)
